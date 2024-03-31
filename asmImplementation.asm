327b09d8d7e7e7481c322e5d6d9f01fc69050ff4
Dane Chan <114413054+TheBanana03@users.noreply.github.com>
2024-03-31 23:23:17 +0800

    removed print

--- asmImplementation.asm
+++ asmImplementation.asm
@@ -8,25 +8,19 @@ Y dq 11.0, 12.0, 13.0
 Z dq 0.0, 0.0, 0.0
 
 n dq 3
-temp dq 0
 
-msg dq "Answer: %1f", 0
-msgExt dq ", %1f", 0
-
-section .text
-extern printf
+section .text\
 
 global main
 main:
     mov rbp, rsp; for correct debugging
     ;write your code here
-    sub rsp, 8*5
     mov r8, [n]
     sub r8, 0x1
     
     L1:
         cmp r8, 0x0
-        jl printFirst
+        jl Tapos
                     
         movsd xmm1, [A + r8*8]
         movsd xmm2, [X + r8*8]
@@ -40,47 +34,7 @@ main:
         dec r8
         
         jmp L1
-   
-    printFirst:
-        mov r8, [n]
-        
-        movsd xmm1, [Z]
-        movq rdx, xmm1
-        lea rcx, [msg]
-        
-        call printf
-        
-        add rsp, 8*5
-        
-        mov qword [temp], 0x1
-        mov r9, [temp]
-        
-        jmp Print
-    
-    Print:
-        mov r9, [temp]
-    
-        cmp r9, [n]
-        jge Tapos
-        
-        sub rsp, 8*5
-        
-        mov r9, [temp]
-        movsd xmm1, [Z + r9*8]
-        movq rdx, xmm1
-        lea rcx, [msgExt]
-        call printf
-        
-        add rsp, 8*5
-        
-        mov r9, [temp]
-        inc r9
-        mov qword [temp], r9
-        
-        jmp Print
-        
     Tapos:
-        ;add rsp, 8*5
         
     xor rax, rax
     ret
